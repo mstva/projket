009e53473678de0a93e3cc6499538f11e29fb93e
mstva <mstva@gmail.com>
2021-01-13 18:35:49 +0200

    setup models relationships

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  belongs_to :user
+  belongs_to :account
   belongs_to :team
 
   accepts_nested_attributes_for :team
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
-  has_many :users
+  has_many :accounts
   has_many :projects
 
-  accepts_nested_attributes_for :users, allow_destroy: true
+  accepts_nested_attributes_for :accounts, allow_destroy: true
 end
